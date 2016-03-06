7cb5648f0d30f2ed4d5362b6d6f25c84fe34192e
Fernando Oliveira <fernando@fholiveira.com>
2016-03-06 11:42:06 -0300

    Fix test

--- Test.hs
+++ Test.hs
@@ -8,10 +8,10 @@ main = hspec $ do
     it "should return all combinations" $ do
       shouldBe 1260 . length $ group [2, 3, 4] ("ABCDEFGHI" :: [Char])
     it "should return all combinations" $ do
-      shouldBe 760 . length $ group [2, 2, 5] ("ABCDEFGHI" :: [Char])
+      shouldBe 756 . length $ group [2, 2, 5] ("ABCDEFGHI" :: [Char])
     it "should return" $ do
-      group [2, 3] "ABCDE" `shouldBe` [
-          ["AB" :: [Char], "CDE"], ["AC", "BDE"], ["AD", "BCE"], ["AE", "BCD"],
-          ["BC", "ADE"], ["BD", "ACE"], ["BE", "ACD"],
-          ["CD", "ABE"], ["CE", "ABD"],
-          ["DE", "ABC"]]
+      group [2, 3] "ABCDE" `shouldBe`
+        [["AB" :: [Char], "CDE"], ["AC", "BDE"], ["AD", "BCE"], ["AE", "BCD"],
+         ["BC", "ADE"], ["BD", "ACE"], ["BE", "ACD"],
+         ["CD", "ABE"], ["CE", "ABD"],
+         ["DE", "ABC"]]
